3cfaea0f41487e7b9357d601911542946f7abda0
l-amu <lamiekgim@gmail.com>
2021-10-03 14:06:42 -0400

    added random function

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,37 @@
-// Assignment code here
+// // Assignment code here
+
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// // Assignment code here
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+ const generatePassword = {
+  getRandomLower: String.fromCharCode(Math.floor(Math.random() * + 26) + 97);
+  getRandomUpper: String.fromCharCode(Math.floor(Math.random() * + 26) + 65);
+  getRandomNumber: String.fromCharCode(Math.floor(Math.random() * + 10) + 48);
+}
+// console.log(generatePassword());
+function generatePassword() {
+  return String.fromCharCode (Math.random() * + 26 + 97);
+}
+console.log(generatePassword());
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
