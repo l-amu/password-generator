354212c7b6ee61bdf20bb1f24641ef6e22365fa2
l-amu <lamiekgim@gmail.com>
2021-10-08 20:44:27 -0400

    restarting

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,16 @@
-// // Assignment code here
-
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-// // Assignment code here
+// Assignment code here
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
- const generatePassword = {
-  getRandomLower: String.fromCharCode(Math.floor(Math.random() * + 26) + 97);
-  getRandomUpper: String.fromCharCode(Math.floor(Math.random() * + 26) + 65);
-  getRandomNumber: String.fromCharCode(Math.floor(Math.random() * + 10) + 48);
-}
-// console.log(generatePassword());
-function generatePassword() {
-  return String.fromCharCode (Math.random() * + 26 + 97);
-}
-console.log(generatePassword());
 // Write password to the #password input
+var RandomNumber = [1,2,3,4,5,6,7,8,9,10]
+var RandomLowerCase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y,z]
+var RandomUpperCase = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+var RandomSymbol  = [,~,`,!, ,#$%^&*()_-+={[}]|\:;"'<,=,>,.,?,/]
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
